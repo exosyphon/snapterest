650e103b815d1f70cc4db2be4162b677e6842a26
exosyphon <exosyphon@yahoo.com>
2017-01-09 20:23:36 -0700

    Completed live twitter feed app

--- source/app.js
+++ source/app.js
@@ -1,39 +1,5 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Application = require('./components/Application.react');
 
-var statefulComponent = React.createClass({
-    getInitialState: function() {
-        return {
-            isHeaderHidden: false,
-            title: 'Stateful React Component'
-        };
-    },
-
-    handleClick: function() {
-        this.setState({
-            isHeaderHidden: !this.state.isHeaderHidden
-        });
-    },
-
-    render: function() {
-        var headerElement = React.createElement('h1', {
-            className: 'header',
-            key: 'header'
-        }, this.state.title);
-        var buttonElement = React.createElement('button', {
-            className: 'btn btn-default',
-            onClick: this.handleClick,
-            key: 'button'
-        }, 'Toggle header ');
-
-        if (this.state.isHeaderHidden) {
-            return React.createElement('div', null, [buttonElement]);
-        }
-
-        return React.createElement('div', null, [buttonElement,
-            headerElement
-        ]);
-    }
-});
-
-ReactDOM.render(React.createElement(statefulComponent), document.getElementById('react-application'));
+ReactDOM.render(<Application/>, document.getElementById('react-application'));
